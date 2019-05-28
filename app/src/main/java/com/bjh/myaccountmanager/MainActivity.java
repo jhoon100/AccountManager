fc075a4a30666a71842d5be41aaef3ed97c34bdb
gabrielbaek <jhoon100@gmail.com>
2019-05-28 22:22:01 +0900

    working

--- app/src/main/java/com/bjh/myaccountmanager/MainActivity.java
+++ app/src/main/java/com/bjh/myaccountmanager/MainActivity.java
@@ -4,7 +4,6 @@ import android.arch.lifecycle.MutableLiveData;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -73,8 +72,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_calender, container, false);
 
             //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
@@ -88,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -123,8 +121,6 @@ public class MainActivity extends AppCompatActivity {
                 GregorianCalendar calendar = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + i, 1, 0, 0, 0);
 
                 calendarList.add(calendar.getTimeInMillis()); //날짜 타입
-
-
 
                 int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1; //해당 월에 시작하는 요일 -1 을 하면 빈칸을 구할 수 있겠죠 ?
                 int max = calendar.getActualMaximum(Calendar.DAY_OF_MONTH); // 해당 월에 마지막 요일
