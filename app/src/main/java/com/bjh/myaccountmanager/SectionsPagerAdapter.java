a0fb527fe2f24370fe69cb50f403d95ca41f537f
gabrielbaek <jhoon100@gmail.com>
2019-06-02 23:15:18 +0900

    working

---
+++ app/src/main/java/com/bjh/myaccountmanager/SectionsPagerAdapter.java
@@ -0,0 +1,24 @@
+package com.bjh.myaccountmanager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
