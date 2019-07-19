491af3630afcea3889364e23b43e9261a2f62250
gabriel <jhoon100@gmail.com>
2019-07-19 22:53:15 +0900

    타이틀 레이아웃 변경

--- app/src/main/java/com/bjh/myaccountmanager/StatisticsActivity.java
+++ app/src/main/java/com/bjh/myaccountmanager/StatisticsActivity.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.bjh.myaccountmanager.util.StringUtil;
 
@@ -11,13 +12,18 @@ public class StatisticsActivity extends AppCompatActivity {
 
     DatePickerDialog datePickerDialog;
 
+    TextView txtSrhStartDate;
+    TextView txtSrhEndDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
 
-        datePickerDialog =  new DatePickerDialog(this, listener, Integer.valueOf(StringUtil.getCurYear()), Integer.valueOf(StringUtil.getCurMonth())-1, Integer.valueOf(StringUtil.getCurDay()));
+        txtSrhStartDate = (TextView) findViewById(R.id.txtSrhStartDate);
+        txtSrhEndDate = (TextView) findViewById(R.id.txtSrhEndDate);
 
+        datePickerDialog = new DatePickerDialog(this, listener, Integer.valueOf(StringUtil.getCurYear()), Integer.valueOf(StringUtil.getCurMonth())-1, Integer.valueOf(StringUtil.getCurDay()));
         datePickerDialog.show();
     }
 
