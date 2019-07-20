a621602e212731001a8777a648b0eebbbcbcdeac
gabrielbaek <jhoon100@gmail.com>
2019-07-20 19:24:32 +0900

    working

--- app/src/main/java/com/bjh/myaccountmanager/StatisticsActivity.java
+++ app/src/main/java/com/bjh/myaccountmanager/StatisticsActivity.java
@@ -3,6 +3,7 @@ package com.bjh.myaccountmanager;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
@@ -23,8 +24,25 @@ public class StatisticsActivity extends AppCompatActivity {
         txtSrhStartDate = (TextView) findViewById(R.id.txtSrhStartDate);
         txtSrhEndDate = (TextView) findViewById(R.id.txtSrhEndDate);
 
-        datePickerDialog = new DatePickerDialog(this, listener, Integer.valueOf(StringUtil.getCurYear()), Integer.valueOf(StringUtil.getCurMonth())-1, Integer.valueOf(StringUtil.getCurDay()));
+        datePickerDialog =  new DatePickerDialog(this, listener, Integer.valueOf(StringUtil.getCurYear()), Integer.valueOf(StringUtil.getCurMonth())-1, Integer.valueOf(StringUtil.getCurDay()));
+
         datePickerDialog.show();
+
+        // 통계 조회 시작일 선택 시 달력 팝업 실행 후 선택하면 선택된 데이터 입력 처리
+        txtSrhStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // 통계 조회 종료일 선택 시 달력 팝업 실행 후 선택하면 선택된 데이터 입력 처리
+        txtSrhEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
